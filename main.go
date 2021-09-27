4f45d3b2570211d47936f6615dfb16d92cce0bce
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-09-28 00:51:22 +0300

    get absolute path

--- main.go
+++ main.go
@@ -3,38 +3,46 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
+type Key struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
 type Secret struct {
-	Filename string            `json:"filename"`
-	Keys     map[string]string `json:"keys"`
+	Filename string `json:"filename"`
+	Keys     []Key  `json:"keys"`
 }
 
 func main() {
 	var secrets []Secret
 
-	secretTemplate := flag.String("secrets", "", "secrets for replacing")
+	secretTemplate := flag.String("secrets", "", "Replace secrets with given keys")
+	flag.Parse()
 
-	err:=json.Unmarshal([]byte(*secretTemplate), &secrets)
+	err := json.Unmarshal([]byte(*secretTemplate), &secrets)
 
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, v := range secrets {
-		err=PasteSecret(v.Filename, v.Keys)
-		if err!=nil{
+		err = PasteSecret(v.Filename, v.Keys)
+		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
 }
 
-func PasteSecret(filename string, keys map[string]string) error {
-
+func PasteSecret(filename string, keys []Key) error {
+	filename, _ = filepath.Abs(filename)
 	file, err := os.ReadFile(filename)
 	text := string(file)
 
@@ -42,8 +50,8 @@ func PasteSecret(filename string, keys map[string]string) error {
 		return err
 	}
 
-	for key, value := range keys {
-		text = strings.ReplaceAll(text, key, value)
+	for _, key := range keys {
+		text = strings.ReplaceAll(text, fmt.Sprintf("{{%s}}", key.Key), key.Value)
 	}
 
 	err = os.WriteFile(filename, []byte(text), os.ModePerm)
